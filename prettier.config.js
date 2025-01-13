82fe3f2b44faf46f46338281505ec31f46257fd9
Tyson Matanich <tysonmatanich@users.noreply.github.com>
2025-01-13 10:29:34 -0800

    Updating prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,9 +1,18 @@
 // https://prettier.io/docs/en/options.html
 module.exports = {
-  trailingComma: 'es5',
+  trailingComma: "es5",
   bracketSpacing: true,
   tabWidth: 2,
-  semi: false,
-  singleQuote: true,
-  arrowParens: 'always',
-}
+  semi: true,
+  singleQuote: false,
+  arrowParens: "always",
+  endOfLine: "lf",
+  insertPragma: false,
+  printWidth: 80,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  requirePragma: false,
+  useTabs: false,
+  embeddedLanguageFormatting: "auto",
+  experimentalTernaries: false,
+};
--- test/index.test.js
+++ test/index.test.js
@@ -1,25 +1,25 @@
-const test = require('ava')
-const fs = require('fs')
-const util = require('util')
-const sinon = require('sinon')
-const { v4: uuidv4 } = require('uuid')
-const handler = require('..')
+const test = require("ava");
+const fs = require("fs");
+const util = require("util");
+const sinon = require("sinon");
+const { v4: uuidv4 } = require("uuid");
+const handler = require("..");
 
-const writeFile = util.promisify(fs.writeFile)
-const readFile = util.promisify(fs.readFile)
-const unlink = util.promisify(fs.unlink)
+const writeFile = util.promisify(fs.writeFile);
+const readFile = util.promisify(fs.readFile);
+const unlink = util.promisify(fs.unlink);
 
 async function run(content, inputs, otherOptions = {}) {
-  const fileName = `${uuidv4()}.txt`
-  if (!inputs) inputs = {}
-  inputs.include_files = [fileName]
+  const fileName = `${uuidv4()}.txt`;
+  if (!inputs) inputs = {};
+  inputs.include_files = [fileName];
 
   try {
-    await writeFile(fileName, content)
+    await writeFile(fileName, content);
 
     const listAll = sinon
       .stub()
-      .returns([{ runtime: 'js', extension: '.js', srcFile: fileName }])
+      .returns([{ runtime: "js", extension: ".js", srcFile: fileName }]);
 
     const utils = otherOptions.mockUtils
       ? otherOptions.mockUtils(fileName)
@@ -27,89 +27,89 @@ async function run(content, inputs, otherOptions = {}) {
           functions: { listAll },
           build: { failBuild: console.log },
           status: { show: console.log },
-        }
+        };
 
-    const returnCode = await handler.processFiles({ inputs, utils })
+    const returnCode = await handler.processFiles({ inputs, utils });
 
-    const transformedFileContent = await readFile(fileName, 'utf8')
+    const transformedFileContent = await readFile(fileName, "utf8");
 
-    await unlink(fileName)
+    await unlink(fileName);
 
     return {
       returnCode,
       transformedFileContent,
-    }
+    };
   } catch (error) {
-    console.error('Error during test run:', error)
-    await unlink(fileName)
-    throw error
+    console.error("Error during test run:", error);
+    await unlink(fileName);
+    throw error;
   }
 }
 
-test('empty file', async (t) => {
-  t.is((await run('', {})).transformedFileContent, '')
-})
+test("empty file", async (t) => {
+  t.is((await run("", {})).transformedFileContent, "");
+});
 
-test('default inputs', async (t) => {
-  process.env.VAR_1 = 'foo'
-  process.env.VAR_2 = 'bar'
+test("default inputs", async (t) => {
+  process.env.VAR_1 = "foo";
+  process.env.VAR_2 = "bar";
 
   t.is(
     (await run(`() => {process.env.VAR_1;process.env.VAR_2;};`))
       .transformedFileContent,
     `() => {"foo";"bar";};`
-  )
-})
+  );
+});
 
-test('inputs with include_vars', async (t) => {
-  process.env.VAR_1 = 'foo'
-  process.env.VAR_2 = 'bar'
+test("inputs with include_vars", async (t) => {
+  process.env.VAR_1 = "foo";
+  process.env.VAR_2 = "bar";
 
   t.is(
     (
       await run(`() => {process.env.VAR_1;process.env.VAR_2;};`, {
-        include_vars: ['VAR_1'],
+        include_vars: ["VAR_1"],
       })
     ).transformedFileContent,
     `() => {"foo";process.env.VAR_2;};`
-  )
-})
+  );
+});
 
-test('inputs with exclude_vars', async (t) => {
-  process.env.VAR_1 = 'foo'
-  process.env.VAR_2 = 'bar'
-
-  t.is(
-    (
-      await run(`() => {process.env.VAR_1;process.env.VAR_2;};`, {
-        exclude_vars: ['VAR_1'],
-      })
-    ).transformedFileContent,
-    `() => {process.env.VAR_1;"bar";};`
-  )
-})
-
-test('inputs with both include_vars and exclude_vars', async (t) => {
-  process.env.VAR_1 = 'foo'
-  process.env.VAR_2 = 'bar'
+test("inputs with exclude_vars", async (t) => {
+  process.env.VAR_1 = "foo";
+  process.env.VAR_2 = "bar";
 
   t.is(
     (
       await run(`() => {process.env.VAR_1;process.env.VAR_2;};`, {
-        exclude_vars: ['VAR_1'],
-        include_vars: ['VAR_2'],
+        exclude_vars: ["VAR_1"],
       })
     ).transformedFileContent,
     `() => {process.env.VAR_1;"bar";};`
-  )
-})
+  );
+});
 
-test('inputs without buildEvent', async (t) => {
-  t.deepEqual(handler({}), { onPreBuild: handler.processFiles })
-})
+test("inputs with both include_vars and exclude_vars", async (t) => {
+  process.env.VAR_1 = "foo";
+  process.env.VAR_2 = "bar";
 
-test('inputs with buildEvent', async (t) => {
-  t.deepEqual(handler({ buildEvent: 'onBuild' }), {
+  t.is(
+    (
+      await run(`() => {process.env.VAR_1;process.env.VAR_2;};`, {
+        exclude_vars: ["VAR_1"],
+        include_vars: ["VAR_2"],
+      })
+    ).transformedFileContent,
+    `() => {process.env.VAR_1;"bar";};`
+  );
+});
+
+test("inputs without buildEvent", async (t) => {
+  t.deepEqual(handler({}), { onPreBuild: handler.processFiles });
+});
+
+test("inputs with buildEvent", async (t) => {
+  t.deepEqual(handler({ buildEvent: "onBuild" }), {
     onBuild: handler.processFiles,
-  })
-})
+  });
+});
